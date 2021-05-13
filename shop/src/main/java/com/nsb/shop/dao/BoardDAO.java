393c717755a03393aea9ace9833243f3c7d22274
dlstkemfu <dlstkemfu@naver.com>
2021-05-13 19:37:58 +0900

    2021-05-13(1)

---
+++ shop/src/main/java/com/nsb/shop/dao/BoardDAO.java
@@ -0,0 +1,37 @@
+package com.nsb.shop.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nsb.shop.logic.Board;
+
+@Repository
+public class BoardDAO {
+
+	@Autowired
+	public SqlSession sqlSession;
+
+	public List<Board> getBoardList() {
+		return sqlSession.selectList("getBoardList");
+	}
+
+	public int boardwrite(Board board) {
+		return sqlSession.insert("boardwrite", board);
+	}
+
+	public Board getBoardDetail(int id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		return sqlSession.selectOne("getBoardDetail", map);
+	}
+
+	public void viewUpdate(int id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		sqlSession.update("viewUpdate", map);
+	}
+}
---
+++ shop/src/main/java/com/nsb/shop/service/BoardService.java
@@ -0,0 +1,14 @@
+package com.nsb.shop.service;
+
+import java.util.List;
+
+import com.nsb.shop.logic.Board;
+
+public interface BoardService {
+	List getBoardList();
+
+	int boardwrite(Board board);
+	
+	Board getBoardDetail(int id);
+	void viewsUpdate(int id);
+}
